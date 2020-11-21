b00a08defcb9da16cf6ff0c281317bab0526d9bd
Mhıar ALAIOOB <20630424@stu.omu.edu.tr>
2020-11-21 21:46:22 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+ int n1;
+ int n2;
+ int hh;
+ printf("by norsan \n write first number : ");
+ scanf("%d",&n1);
+ printf("write secound number : ");
+ scanf("%d" ,&n2);
+ hh= n1^n2;
+ printf("answar = ");
+ printf("%d" ,hh);
+  return 0;
+}
